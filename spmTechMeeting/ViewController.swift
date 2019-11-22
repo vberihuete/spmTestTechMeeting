b33696baa0030194fe7ee188e59e15c417af2d69
Vincent Berihuete <vincent.berihuete@daresay.co>
2019-11-22 08:37:45 +0100

    initial commit

---
+++ spmTechMeeting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  spmTechMeeting
+//
+//  Created by Vincent Berihuete on 2019-11-22.
+//  Copyright © 2019 Vincent Berihuete. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
